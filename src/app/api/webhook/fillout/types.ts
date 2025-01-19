7970e8b717e6cddafcb20b3e138184ff9073128f
Chipzstar <chipzstar.dev@googlemail.com>
2025-01-19 23:46:36 +0000

    Add prettyPrint utility function and micro dependency + bug fixes

---
+++ src/app/api/webhook/fillout/types.ts
@@ -0,0 +1,90 @@
+type QuestionType =
+  | 'MultipleChoice'
+  | 'Dropdown'
+  | 'ShortAnswer'
+  | 'DatePicker'
+  | 'Checkboxes'
+  | 'StarRating'
+  | 'NumberInput'
+  | 'Slider';
+
+export interface SliderQuestion {
+  type: 'Slider';
+  value: number;
+}
+
+export interface StarRatingQuestion {
+  type: 'StarRating';
+  value: number;
+}
+
+export interface DatePickerQuestion {
+  type: 'DatePicker';
+  value: string;
+}
+
+export interface MultipleChoiceQuestion {
+  type: 'MultipleChoice';
+  value: string;
+}
+
+export interface CheckboxQuestion {
+  type: 'Checkboxes';
+  value: string[];
+}
+
+export interface DropdownQuestion {
+  type: 'Dropdown';
+  value: string;
+}
+
+export interface ShortAnswerQuestion {
+  type: 'ShortAnswer';
+  value: string;
+}
+
+export interface NumberInputQuestion {
+  type: 'NumberInput';
+  value: number;
+}
+
+export type QuestionTypes =
+  | CheckboxQuestion
+  | MultipleChoiceQuestion
+  | DatePickerQuestion
+  | SliderQuestion
+  | StarRatingQuestion
+  | DropdownQuestion
+  | ShortAnswerQuestion
+  | NumberInputQuestion;
+
+export type Question = QuestionTypes & {
+  id: string;
+  name: string;
+};
+
+export interface Submission {
+  submissionId: string;
+  submissionTime: string;
+  lastUpdatedAt: string;
+  questions: Question[];
+}
+
+export interface FormEvent {
+  formId: string;
+  formName: string;
+  submission: Submission;
+}
+
+export type RatingType =
+  | 'overall'
+  | 'interview-process'
+  | 'diversity'
+  | 'team-culture'
+  | 'application-process'
+  | 'recommendation'
+  | 'flexibility'
+  | 'authenticity'
+  | 'work-life-balance';
+
+export type ReviewType = 'Student' | 'Young professional';
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-    import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-    export function cn(...inputs: ClassValue[]) {
-      return twMerge(clsx(inputs))
-    }
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function prettyPrint(obj: any, char = '-') {
+  console.log(char.repeat(40));
+  console.log(obj);
+  console.log(char.repeat(40));
+}
