cbed3b43c141486bb6daece7a4d1f45592f69405
Chipzstar <chipzstar.dev@googlemail.com>
2025-01-27 22:36:01 +0000

    remove async in login + sign-up pages

--- src/app/(auth)/login/[[...login]]/page.tsx
+++ src/app/(auth)/login/[[...login]]/page.tsx
@@ -1,12 +1,5 @@
 import { SignIn, useUser } from '@clerk/nextjs';
-import { currentUser } from '@clerk/nextjs/server';
 
-export default async function Login() {
-  const user = await currentUser();
-
-  if (!user) {
-    return <SignIn />;
-  }
-
-  return <div>Welcome!</div>;
+export default function Login() {
+  return <SignIn />;
 }
--- src/app/(auth)/signup/[[...signup]]/page.tsx
+++ src/app/(auth)/signup/[[...signup]]/page.tsx
@@ -1,12 +1,5 @@
 import { SignUp } from '@clerk/nextjs';
-import { currentUser } from '@clerk/nextjs/server';
 
-export default async function Signup() {
-  const user = await currentUser();
-
-  if (!user) {
-    return <SignUp />;
-  }
-
-  return <div>Welcome!</div>;
+export default function Signup() {
+  return <SignUp />;
 }
