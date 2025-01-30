25200d0955660ff6e669487877ede5305d7dddbd
Chipzstar <chipzstar.dev@googlemail.com>
2025-01-30 22:28:01 +0000

    cleanup edit survey form

--- src/app/(app)/survey/[id]/page.tsx
+++ src/app/(app)/survey/[id]/page.tsx
@@ -2,46 +2,24 @@ import { Button } from '~/components/ui/button';
 import Link from 'next/link';
 import { auth } from '@clerk/nextjs/server';
 import { trpc } from '~/trpc/server';
-import { differenceInMinutes, differenceInSeconds, format } from 'date-fns';
-import { DataTable } from '~/components/leaderboard/data-table';
-import { columns } from '~/components/leaderboard/columns';
-import { MainNav } from '~/components/layout/main-nav';
+import { differenceInMinutes, format } from 'date-fns';
 
 export default async function SurveyDashboard({ params }: { params: { id: string } }) {
   const { id } = params;
   const user = await auth();
 
   const survey = await trpc.survey.byIdWithAnalytics({ id: Number(id) });
-
-  // Statistics
   const surveys_completed = survey.responses.filter(response => response.is_completed);
   const completion_rate = ((surveys_completed.length / survey.responses.length) * 100).toFixed(1);
+
   const total_completion_time = surveys_completed.reduce((acc, response) => {
     const diff = differenceInMinutes(new Date(response.completed_at), new Date(response.started_at));
     // console.log(format(response.started_at, 'hh:mm:ss'));
     // console.log(format(response.completed_at, 'hh:mm:ss'));
     return acc + diff;
   }, 0);
+
   const avg_completion_time = total_completion_time / surveys_completed.length;
-
-  // Data
-  const sortedResponses = survey.responses.sort((a, b) => {
-    const timeA = differenceInSeconds(new Date(a.completed_at), new Date(a.started_at));
-    const timeB = differenceInSeconds(new Date(b.completed_at), new Date(b.started_at));
-    return timeA - timeB; // Sort ascending (lowest to highest)
-  });
-  const data = sortedResponses.map((response, index) => {
-    const completion_time = differenceInSeconds(new Date(response.completed_at), new Date(response.started_at));
-    return {
-      rank: index + 1,
-      name: response.user.firstname,
-      time: completion_time,
-      ref: survey.referrals.filter(r => r.referrer_id === response.user_id).length ?? 0,
-      total: response.points_earned
-    };
-  });
-
-  const isSurveyClosed = survey.end_date < new Date();
 
   if (!survey) {
     return (
@@ -58,43 +36,104 @@ export default async function SurveyDashboard({ params }: { params: { id: string
   }
 
   return (
-    <div className='mx-auto flex w-full max-w-3xl flex-col'>
-      <section className='flex flex-col'>
-        <h2 className='mb-2 text-2xl font-bold md:mb-4'>The Big Picture</h2>
-        <div className='mb-2 flex gap-4 md:mb-4 md:gap-8'>
-          <div className='flex flex-col items-center'>
-            <h3 className='text-xl font-semibold'>Completed surveys</h3>
-            <p className='text-2xl font-bold md:text-3xl'>{surveys_completed.length}</p>
+    <main className='flex min-h-screen flex-col items-center bg-gradient-to-br from-primary to-secondary p-4 text-white md:p-24'>
+      <div className='mb-4 flex w-full items-center justify-between md:mb-8'>
+        <h1 className='text-3xl font-bold md:text-4xl'>The Igbo Gala 2025</h1>
+        <Link href='/' className='text-white underline'>
+          Home
+        </Link>
+      </div>
+      <div className='flex flex-col items-center'>
+        <div className='mb-4 md:mb-8'>
+          <Button asChild>
+            <Link href={`/survey/${id}/share`}>QR code & link</Link>
+          </Button>
+        </div>
+        <div className='flex flex-col items-start'>
+          <h2 className='mb-2 text-2xl font-bold md:mb-4'>The Big Picture</h2>
+          <div className='mb-2 flex gap-4 md:mb-4 md:gap-8'>
+            <div className='flex flex-col items-center'>
+              <h3 className='text-xl font-semibold'>Completed surveys</h3>
+              <p className='text-2xl font-bold md:text-3xl'>{surveys_completed.length}</p>
+            </div>
+            <div className='flex flex-col items-center'>
+              <h3 className='text-xl font-semibold'># of referrals</h3>
+              <p className='text-2xl font-bold md:text-3xl'>{survey.referrals.length}</p>
+            </div>
           </div>
-          <div className='flex flex-col items-center'>
-            <h3 className='text-xl font-semibold'># of referrals</h3>
-            <p className='text-2xl font-bold md:text-3xl'>{survey.referrals.length}</p>
+          <div className='mb-2 flex flex-col gap-1 md:mb-4 md:gap-2'>
+            <p>
+              No. of surveys started: <span className='font-bold'>{survey.responses.length}</span>
+            </p>
+            <p>
+              No. of surveys completed: <span className='font-bold'>{surveys_completed.length}</span>
+            </p>
+            <p>
+              Survey completion rate: <span className='font-bold'>{completion_rate}%</span>
+            </p>
+            <p>
+              Average time taken: <span className='font-bold'>{avg_completion_time}mins</span>
+            </p>
+            <p>
+              Total no. of referrals: <span className='font-bold'>{survey.referrals.length}</span>
+            </p>
+            <p>
+              Av. number of referrals: <span className='font-bold'>5</span>
+            </p>
+          </div>
+          <h2 className='mb-2 text-2xl font-bold md:mb-4'>Giftcard Leaderboard</h2>
+          <div className='overflow-x-auto'>
+            <table className='w-full table-auto'>
+              <thead>
+                <tr>
+                  <th className='px-2 py-1 md:px-4 md:py-2'></th>
+                  <th className='px-2 py-1 md:px-4 md:py-2'>Time(m)</th>
+                  <th className='px-2 py-1 md:px-4 md:py-2'>Ref</th>
+                  <th className='px-2 py-1 md:px-4 md:py-2'>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>1. Chioma Abazie</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>2:10</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>5</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>590</td>
+                </tr>
+                <tr>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>2. Shake Belton</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>2:14</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>3</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>470</td>
+                </tr>
+                <tr>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>3. Berry Coolio</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>2:16</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>2</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>450</td>
+                </tr>
+                <tr>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>4. Julie Ashray</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>2:18</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>1</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>430</td>
+                </tr>
+                <tr>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>5. Mary Look</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>3:00</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>3</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>425</td>
+                </tr>
+                <tr>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>6. Daniel Asher</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>3:15</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>3</td>
+                  <td className='border px-2 py-1 md:px-4 md:py-2'>400</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
-        <div className='mb-2 flex flex-col gap-1 md:mb-4 md:gap-2'>
-          <p>
-            No. of surveys started: <span className='font-bold'>{survey.responses.length}</span>
-          </p>
-          <p>
-            No. of surveys completed: <span className='font-bold'>{surveys_completed.length}</span>
-          </p>
-          <p>
-            Survey completion rate: <span className='font-bold'>{completion_rate}%</span>
-          </p>
-          <p>
-            Average time taken: <span className='font-bold'>{avg_completion_time}mins</span>
-          </p>
-          <p>
-            Total no. of referrals: <span className='font-bold'>{survey.referrals.length}</span>
-          </p>
-        </div>
-      </section>
-      <section className='mt-5 flex flex-col'>
-        <h2 className='mb-2 text-2xl font-bold md:mb-4'>Gift card Leaderboard</h2>
-        <div className='w-full overflow-x-auto'>
-          <DataTable columns={columns} data={data} />
-        </div>
-      </section>
-    </div>
+      </div>
+    </main>
   );
 }
--- src/trpc/routers/survey.ts
+++ src/trpc/routers/survey.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter, protectedProcedure } from '../init';
-import { surveyResponseTable, surveyTable } from '~/db/schema';
+import { giftCardTable, surveyResponseTable, surveyTable, usersTable } from '~/db/schema';
 import { z } from 'zod';
 import { TRPCError } from '@trpc/server';
 import { eq } from 'drizzle-orm';
@@ -37,5 +37,40 @@ export const surveyRouter = createTRPCRouter({
 
     console.log(survey);
     return survey[0];
+  }),
+  update: protectedProcedure.input(z.object({ id: z.number() })).mutation(async ({ ctx, input }) => {
+    const { id } = input;
+    try {
+      const user = await ctx.db.select().from(usersTable).where(eq(usersTable.id, id));
+      if (!user[0]) throw new TRPCError({ code: 'NOT_FOUND' });
+      const survey = await ctx.db.select().from(surveyTable).where(eq(surveyTable.id, id));
+      if (!survey[0]) throw new TRPCError({ code: 'NOT_FOUND' });
+      const { points, referral_bonus_points, number_of_winners, end_date, giftCards } = input;
+      await ctx.db
+        .update(surveyTable)
+        .set({
+          points,
+          referral_bonus_points,
+          number_of_winners,
+          end_date
+        })
+        .where(eq(surveyTable.id, id));
+      if (giftCards) {
+        // await ctx.db.delete().from(giftCardTable).where(eq(giftCardTable.survey_id, id));
+        for (const giftCard of giftCards) {
+          await ctx.db.insert(giftCardTable).values({
+            survey_id: id,
+            name: giftCard.name,
+            code: giftCard.code,
+            expiry_date: giftCard.expiry_date,
+            value: giftCard.value
+          });
+        }
+      }
+      return survey[0];
+    } catch (error) {
+      console.error(error);
+      throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR' });
+    }
   })
 });
