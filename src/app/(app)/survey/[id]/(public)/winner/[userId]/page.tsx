efe56473c0504edd4d8cd6b2605cbce69f188936
Chipzstar <chipzstar.dev@googlemail.com>
2025-02-14 23:11:10 +0000

    minor bug fixes

--- src/app/(app)/survey/[id]/(public)/winner/[userId]/page.tsx
+++ src/app/(app)/survey/[id]/(public)/winner/[userId]/page.tsx
@@ -14,16 +14,18 @@ export default async function WinnerDetailsPage({ params }: { params: { id: stri
   return (
     <HydrateClient>
       <main className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-primary to-secondary p-4 text-white md:p-24'>
-        <div className='flex w-full max-w-3xl flex-col items-center rounded-lg bg-white/10 p-6 shadow-lg'>
+        <div className='flex w-full max-w-3xl flex-col flex-wrap items-center rounded-lg bg-white/10 p-6 shadow-lg'>
           <h2 className='mb-4 text-center text-3xl font-bold drop-shadow-md md:text-4xl'>Congratulations! 🎉</h2>
           <p className='mb-6 text-center text-2xl'>You have won the Gift Card! 🥳</p>
           <div className='mb-4 rounded-md bg-blue-500 px-16 py-12 text-center'>
             <p className='text-3xl font-bold'>{giftCardDetails.brand}</p>
           </div>
           <p className='mb-2'>Amount: £{giftCardDetails.value}</p>
-          <Link href={giftCardDetails.code} className='mb-2 text-wrap' target={'_blank'}>
-            {giftCardDetails.code}
-          </Link>
+          <p className='mb-2 text-wrap underline'>
+            <Link href={giftCardDetails.code} target={'_blank'}>
+              {giftCardDetails.code}
+            </Link>
+          </p>
           <p>Expiry date: {format(new Date(giftCardDetails.expiry_date), 'dd/MM/yyyy')}</p>
         </div>
         <div className='flex flex-col items-center gap-4 text-center'>
