736177f8709e5f8c964961c994892e66945fbb39
Chipzstar <chipzstar.dev@googlemail.com>
2025-02-27 11:54:30 +0000

    add thumbnail to share qr code

--- src/app/(app)/survey/[id]/(protected)/share/page.tsx
+++ src/app/(app)/survey/[id]/(protected)/share/page.tsx
@@ -1,19 +1,29 @@
 import { HydrateClient, trpc } from '~/trpc/server';
 import { SurveyLink } from '~/app/(app)/survey/[id]/(protected)/share/survey-link';
+import { cn } from '~/lib/utils';
+import Image from 'next/image';
 
 export default async function EventSharePage({ params }: { params: { id: string } }) {
   const { id } = params;
 
   const survey = await trpc.survey.byIdWithAnalytics({ id: Number(id) });
+  const thumbnails = survey.rewards.filter(r => r.thumbnail).map(r => r.thumbnail) as string[];
 
   return (
     <HydrateClient>
-      <div className='overflow-x-hidden py-12 text-white md:my-auto'>
-        <div className='flex flex-col items-center space-y-6'>
-          <h2 className='text-balance text-center text-xl font-semibold text-[#6F42FF]'>
-            🎉 Scan NOW and let the games begin! 🏆
-          </h2>
-          <SurveyLink surveyId={survey.id} surveyLink={survey.link} />
+      <div className='mx-auto max-w-3xl overflow-x-hidden py-12 text-white md:my-auto'>
+        <div
+          className={cn('grid grid-cols-1 place-items-center', {
+            'lg:grid-cols-2 lg:gap-x-24': thumbnails.length
+          })}
+        >
+          <section className={cn(thumbnails[0] ? 'block items-center' : 'hidden')}>
+            <Image src={thumbnails[0]} alt='thumbnail' width={200} height={200} />
+          </section>
+          <section className='flex flex-col items-center space-y-6'>
+            <h2 className='text-balance text-center text-xl font-semibold text-[#6F42FF]'>🎉 Scan NOW!!! 🏆</h2>
+            <SurveyLink surveyId={survey.id} surveyLink={survey.link} />
+          </section>
         </div>
       </div>
     </HydrateClient>
