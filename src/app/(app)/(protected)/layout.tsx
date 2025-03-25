8438c2a9e4b8a8d68560ba851e1c1f8a502e18fb
Chipzstar <chipzstar.dev@googlemail.com>
2025-03-25 13:52:12 +0000

    minor UI updates

--- src/app/(app)/(protected)/layout.tsx
+++ src/app/(app)/(protected)/layout.tsx
@@ -18,8 +18,10 @@ export default async function SurveyLayout({ children, params }: Props) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className='flex h-full min-h-screen w-full grow flex-col items-center overflow-y-auto p-4 sm:h-screen 2xl:p-12'>
-        <SidebarTrigger />
+      <main className='flex h-full min-h-screen w-full grow flex-col overflow-y-auto p-4 sm:h-screen'>
+        <div className='dark:border-border-dark absolute left-0 right-0 top-0 z-10 h-16 w-full border-b border-border bg-background/60 backdrop-blur-sm dark:bg-background/80 sm:px-4 md:px-6 lg:px-8'>
+          <SidebarTrigger />
+        </div>
         {children}
       </main>
     </SidebarProvider>
