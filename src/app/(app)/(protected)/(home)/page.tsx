54e9d3488f8635e6adc82e4172cd0a0474632194
Chipzstar <chipzstar.dev@googlemail.com>
2025-04-16 13:49:00 +0100

    Fixes mobile positioning of delete button.

--- src/app/(app)/(protected)/(home)/page.tsx
+++ src/app/(app)/(protected)/(home)/page.tsx
@@ -108,7 +108,7 @@ export default async function Dashboard({
                         </div>
                       </div>
                     </Link>
-                    <div className='absolute right-4 top-4 md:bottom-4'>
+                    <div className='absolute right-4 top-4 md:bottom-2 md:top-auto'>
                       <DeleteEventDialog eventId={event.id} eventName={event.name} />
                     </div>
                   </div>
